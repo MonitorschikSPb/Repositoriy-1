a21325aaf38fe42095cd5369e08f11f54b3bcabd
MonitorschikSPb <lelgridnev@gmail.com>
2024-09-26 21:57:21 +0300

    Hello

--- lesson_1.py
+++ lesson_1.py
@@ -1,6 +1,12 @@
+"""
 import math
 a = int(input())
 b = 2*a*math.pi
 c = math.pi*a**2
 print(round(b, 2))
 print(round(c, 2))
+"""
+import math
+L = int(input())
+T = 2*math.pi*(L/9.81)**(1/2)
+print(round(T, 2))
