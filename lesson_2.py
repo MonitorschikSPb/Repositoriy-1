0139401201fefc84c280331e770e77ae774712e3
MonitorschikSPb <lelgridnev@gmail.com>
2024-09-26 23:12:46 +0300

    Hello

---
+++ lesson_2.py
@@ -0,0 +1,17 @@
+"""
+a = int(input())
+b=int(input())
+if b!=0 and a!=0:
+    c=a/b
+    print(c)
+else:
+    print('Error!')
+
+a=int(input())
+if a>20:
+    b=(a/10)*3.5
+    print(a)
+    print(round(b, 2))
+else:
+    print('No discount')
+    """
