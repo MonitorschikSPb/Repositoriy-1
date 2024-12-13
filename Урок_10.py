f84509dab90d868e2f907af31faa46506783f6e4
MonitorschikSPb <lelgridnev@gmail.com>
2024-12-13 10:42:48 +0300

    Привет!

--- Урок_10.py
+++ Урок_10.py
@@ -1,27 +1,23 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-#Создание объекта Series
-s1 = pd.Series([1, 2, 3, 4, 5])
-#Создание объекта DataFrame из списка
-df1 = pd.DataFrame({'A': [1, 2, 3, 4, 5], 'B': [6, 7, 8, 9, 10]})
-#Создание объекта DataFrame из массива NumPy
-arr = np.random.randint(1, 10, (5, 2))
-df2 = pd.DataFrame(arr, columns=['C', 'D'])
-#Получение не пересекающихся элементов в двух объектах Series
-s2 = pd.Series([4, 5, 6, 7, 8])
-diff_s = pd.concat([s1[~s1.isin(s2)], s2[~s2.isin(s1)])
-#Получение не пересекающихся элементов в двух столбцах одного DataFrame
-df_diff = df1.loc[~df1['A'].isin(df2['C'])]
-#Частота уникальных элементов в объекте Series
-s3 = pd.Series([1, 2, 2, 3, 3, 3, 4, 4, 4, 4])
-freq = s3.value_counts()
-freq.plot(kind='bar')
-plt.show()
-#Объединение двух DataFrames вертикально
-df_concat_v = pd.concat([df1, df2], axis=0)
-#Объединение двух DataFrames горизонтально
-df_concat_h = pd.concat([df1, df2], axis=1)
-#График зависимости одного столбца от другого для DataFrame
-df1.plot(x='A', y='B', kind='scatter')
-plt.show()
+
+# 1 (3)
+# ser = pd.Series([1, 2, 3, 4, 3, 4, 4, 5, 5, 5, 5])
+# uniValues = ser.value_counts()
+# print(type(uniValues))
+# plt.bar(uniValues.index, uniValues.values)
+# plt.show()
+
+# 2 (4)
+# df1 = pd.DataFrame({"one" : [1, 2, 43, 21, 21], "two" : [23, 324, 546, 11, 90]})
+# df2 = pd.DataFrame({"three" : [1, 2, 43, 21, 4], "one" : [23, 324, 4, 11, 90]})
+
+# dfOb1 = pd.concat([df1, df2])
+# print(dfOb1)
+# dfOb2 = pd.concat([df1, df2], axis=1)
+# print(dfOb2)
+
+# 3 (5)
+# df = pd.DataFrame({"one" : [1, 2, 43, 21, 21], "two" : [23, 324, 546, 11, 90]})
+# plt.plot(df["one"], df["two"])
+# plt.show()
